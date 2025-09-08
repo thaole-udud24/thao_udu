1d8af5eff7067ff5b85471b76ecd9261b0251e15
lê thị thảo - b24dcc253 <lethithao.ptit@gmail.com>
2025-09-08 22:37:47 +0700

    Update Hinhchunhat.java

--- java/Hinhchunhat.java
+++ java/Hinhchunhat.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-public class J01001 {
+public class Main {
+
+    public static int ChuVi(int x, int y) {
+        return 2 * (x + y);
+    }
+
+    public static int DienTich(int x, int y) {
+        return x * y;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.close();
-        if (a > 0 && b > 0) {
-           System.out.println(((a + b) * 2) + " " + (a * b));
-        } else {
-            System.out.println(0);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println(ChuVi(x, y));
+            System.out.println(DienTich(x, y));
         }
+        sc.close();
     }
 }
